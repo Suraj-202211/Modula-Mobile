156ec1f0175c6b57cb005ceb1d70a6d27bba9912
Sentrik CTO <cto@sentrik.com>
2026-08-23 07:40:04 +0530

    chore: prepare v1.0.7 release with bsdiff compatibility fix

--- scripts/create_manifest.py
+++ scripts/create_manifest.py
@@ -53,12 +53,41 @@ if patch_exists:
     # For now we'll just parse the old version from the patch name (launcher-vOLD_to_vNEW.patch)
     try:
         old_v = patch_name.split('_to_')[0].replace('launcher-v', '')
-        old_numeric = ''.join(filter(str.isdigit, old_v))
-        manifest['files'][0]['patch_for_version_code'] = int(old_numeric) if old_numeric else 1
+        old_parts = old_v.split('.')
+        if len(old_parts) >= 3:
+            manifest['files'][0]['patch_for_version_code'] = int(old_parts[-1])
+        else:
+            old_numeric = ''.join(filter(str.isdigit, old_v))
+            manifest['files'][0]['patch_for_version_code'] = int(old_numeric) if old_numeric else 1
     except:
         pass
 
+import base64
+
+json_string = json.dumps(manifest)
+base64_content = base64.b64encode(json_string.encode('utf-8')).decode('utf-8')
+
+# The broken v1.0.5 client expects a GithubContentApi response when downloading release.json directly
+github_api_mock = {
+    "_links": {
+        "git": "",
+        "html": "",
+        "self": ""
+    },
+    "content": base64_content,
+    "download_url": "",
+    "encoding": "base64",
+    "git_url": "",
+    "html_url": "",
+    "name": "release.json",
+    "path": "release.json",
+    "sha": "",
+    "size": len(base64_content),
+    "type": "file",
+    "url": ""
+}
+
 with open('release.json', 'w') as f:
-    json.dump(manifest, f, indent=2)
+    json.dump(github_api_mock, f, indent=2)
 
 print('release.json generated')
